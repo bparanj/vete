8d7a514d4431bb9b228f6c178b2b22dc9beb530f
Bala Paranj <bparanj@gmail.com>
2017-04-15 19:51:28 -0700

    Added validation tests for the doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,8 @@
 class Doctor < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 35 }
+  validates :zip, length: { maximum: 5, too_long: 'is too long (maximum is 5 digits)'}
+  
+  validates_inclusion_of :experience, :in => 1..100
+  
 end
--- test/models/doctor_test.rb
+++ test/models/doctor_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class DoctorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "doctor name is required" do
+    doctor = Doctor.new
+    doctor.save
+    
+    assert_equal ["can't be blank"], doctor.errors[:name]
+  end
+  
+  test "doctor name cannot exceed 35 characters" do
+    name = 'n' * 36
+    doctor = Doctor.new(name: name)
+    doctor.save
+    
+    assert_equal ["is too long (maximum is 35 characters)"], doctor.errors[:name]
+  end
+  
+  test "doctor with valid name" do
+    name = 'n' * 35
+    doctor = Doctor.new(name: name)
+    doctor.save
+    
+    assert_equal [], doctor.errors[:name]
+  end
+
+  test "doctor zip cannot exceed 5 digits" do
+    name = 'n' * 35
+    zip = '123456'
+    doctor = Doctor.new(name: name, zip: zip)
+    doctor.save
+    
+    assert_equal ["is too long (maximum is 5 digits)"], doctor.errors[:zip]
+  end
+  
+  test "doctor zip is 5 digits" do
+    name = 'n' * 35
+    zip = '12345'
+    doctor = Doctor.new(name: name, zip: zip)
+    doctor.save
+    
+    assert_equal [], doctor.errors[:zip]
+  end
+
+  test "doctor experience is between 1-100" do
+    name = 'n' * 35
+    zip = '12345'
+    experience = 10
+    doctor = Doctor.new(name: name, zip: zip, experience: experience)
+    doctor.save
+    
+    assert_equal [], doctor.errors[:experience]
+  end
+
+  test "doctor experience cannot exceed 100" do
+    name = 'n' * 35
+    zip = '12345'
+    experience = 101
+    doctor = Doctor.new(name: name, zip: zip, experience: experience)
+    doctor.save
+    
+    assert_equal ["is not included in the list"], doctor.errors[:experience]
+  end
+    
 end
