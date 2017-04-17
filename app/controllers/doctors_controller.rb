f4d78d684330d7abeaba033fb1f7d9b710a9cbc5
Bala Paranj <bparanj@gmail.com>
2017-04-17 12:40:09 -0700

    Create and update doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,41 @@
 class DoctorsController < ApplicationController
   def index
+    @doctors = Doctor.all
   end
 
   def show
+    @doctor = Doctor.find(params[:id])
   end
 
   def new
+    @doctor = Doctor.new
+  end
+  
+  def create
+    doctor = Doctor.new(doctor_params)
+    if doctor.save
+      redirect_to doctor_path(doctor), notice: 'Vet created'
+    else
+      flash.notice = "Error occurred"
+
+      render :new
+    end
+  end
+  
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    doctor = Doctor.find(params[:id])
+    doctor.update_attributes(doctor_params)
+
+    redirect_to doctor_path(doctor), notice: 'Doctor updated'
+  end
+  
+  private
+  
+  def doctor_params
+    params.require(:doctor).permit(:name, :address, :city, :state, :zip, :school, :experience)
   end
 end
